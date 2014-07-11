83a8e5c0972d64e994c3868c218e77ea8065fa44
Trent Albright <talbright@desk.com>
2014-07-10 22:08:33 -0400

    Built-in Directives 1.5

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 (function() {
   var app = angular.module('store',[]);
   app.controller('StoreController', function() {
-    this.product = gem;
+    this.products = gems;
   });
-  var gem =  {
-    name: 'Dodecahedron',
-    price: 2.95,
-    description: 'A cool Dodecahedron'
-  }
+  var gems =  [
+    {
+      name: 'Dodecahedron',
+      price: 2.95,
+      description: 'A cool Dodecahedron',
+      canPurchase: true,
+      soldOut: false
+    },
+    {
+      name: 'Pentagonal Gem',
+      price: 3.95,
+      description: 'A cool Pentagonal',
+      canPurchase: true,
+      soldOut: false
+    }
+  ];
 })();
