1141901b9e5492b55c5ce153fdb5517f59e34009
Trent Albright <talbright@desk.com>
2014-07-12 17:34:04 -0400

    Module Refactoring 5.2

--- app.js
+++ app.js
@@ -1,72 +1,15 @@
 (function() {
-  var app = angular.module('gemStore', []);
+  var app = angular.module('gemStore', ['product-directives']);
 
   app.controller('StoreController', function() {
     this.products = gems;
   });
 
-  app.directive("productTabs", function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'product-tabs.html',
-      controllerAs: 'tab',
-      controller: function() {
-      	this.tab = 1;
-
-    		this.isSet = function(checkTab) {
-      		return this.tab === checkTab;
-    		};
-
-    		this.setTab = function(setTab) {
-      		this.tab = setTab;
-    		};
-    	}
-    };
-  });
-
-  app.directive('productGallery', function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'product-gallery.html',
-      controller: function() {
-        this.current = 0;
-    		this.setCurrent = function(imageNumber){
-      		this.current = imageNumber || 0;
-        };
-      },
-      controllerAs: 'gallery'
-    };
-  });
-
-  app.controller("ReviewController", function(){
-
+  app.controller('ReviewController', function(){
     this.review = {};
-
     this.addReview = function(product){
       product.reviews.push(this.review);
       this.review = {};
-    };
-
-  });
-
-  app.directive("productDescription", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "product-description.html"
-    };
-  });
-
-  app.directive("productReviews", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "product-reviews.html"
-    };
-  });
-
-  app.directive("productSpecs", function() {
-    return {
-      restrict:"A",
-      templateUrl: "product-specs.html"
     };
   });
 
